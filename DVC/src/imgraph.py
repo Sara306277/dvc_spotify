3c15a07d8eba5316abe8c155e858da809744f92d
Sara306277 <73878502+Sara306277@users.noreply.github.com>
2020-12-20 14:12:56 +0100

    Update imgraph.py

--- DVC/src/imgraph.py
+++ DVC/src/imgraph.py
@@ -14,7 +14,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from random import choice
 
-#params = yaml.safe_load(open('params.yaml'))['prepare']
 
 if len(sys.argv) != 2:
     sys.stderr.write("Arguments error. Usage:\n")
@@ -25,17 +24,6 @@ os.makedirs(os.path.join('data', 'described'), exist_ok=True)
 
 data = pd.read_csv(sys.argv[1])
 
-
-#with open(sys.argv[1]) as f:
-#    js_graph = json.load(f)
-
-#g = nx.Graph(json_graph.node_link_graph(js_graargvph))
-
-#df = pd.DataFrame(js_graph["nodes"])
-
 data_describe = data.describe()
-#df_csv = df.to_csv(index=False)
 
-#path = os.path.join('data', 'prepared', 'elaborated_data.json')
-#json.dump(output_data, open(path, 'w+'))
 data_describe.to_csv(os.path.join('data/described/outputdesc.csv'))
