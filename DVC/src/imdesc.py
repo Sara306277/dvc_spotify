d91fe294b294fd6348a3a1a89263edb4f7c1e001
Sara306277 <73878502+Sara306277@users.noreply.github.com>
2020-12-20 14:11:44 +0100

    Update imdesc.py

--- DVC/src/imdesc.py
+++ DVC/src/imdesc.py
@@ -14,8 +14,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from random import choice
 
-#params = yaml.safe_load(open('params.yaml'))['prepare']
-
 if len(sys.argv) != 2:
     sys.stderr.write("Arguments error. Usage:\n")
     sys.stderr.write("\tpython imgraph.py data-file\n")
@@ -28,7 +26,4 @@ data = pd.read_csv(sys.argv[1])
 fig, axes = plt.subplots(figsize = (12,12))
 data.hist(ax = axes)
 
-#data_describe = data.describe()
-
-#data_describe.to_csv(os.path.join('data/described_graph/outputdesc.csv'))
 fig.savefig(os.path.join('data/described_graph/desc_graph.png')) 
